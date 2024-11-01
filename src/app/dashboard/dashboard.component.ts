cdd1235e025273d13ad3359e8bd95d4457fa166b
Adriano Shineyder <adrianoshineyder@hotmail.com>
2024-11-01 11:35:59 -0300

    Template básico

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class DashboardComponent {
+
+}
